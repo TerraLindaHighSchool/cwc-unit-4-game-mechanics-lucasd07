19620a96e755c356cc73fdb1e6fac39ad7d8f434
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-02-25 15:24:23 -0800

    Tutorial 4.1.6 Completed

---
+++ Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+    [SerializeField] private float speed;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+    }
+}
--- Prototype 4/Assets/Course Library/Scripts/RotateCamera.cs
+++ Prototype 4/Assets/Course Library/Scripts/RotateCamera.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed;
+    private float horizontalInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 
     // Update is called once per frame
